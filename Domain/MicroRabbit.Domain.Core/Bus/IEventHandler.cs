754ee392447eb6d7ba137a0942b04f4ec7f484f8
Renan Santos <renanwillian87@gmail.com>
2020-06-14 14:53:33 -0300

    Implemented Interfaces and Classes for Domain.Core

---
+++ Domain/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,15 @@
+using MicroRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ Domain/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,14 @@
+using MicroRabbit.Domain.Core.Events;
+using System;
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        
+        protected Command()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
