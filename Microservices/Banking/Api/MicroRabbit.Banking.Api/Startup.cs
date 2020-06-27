e99a3949c6f8ad6c92f8813f9e7811601def5808
Renan Santos <renanwillian87@gmail.com>
2020-06-26 22:40:03 -0300

    Implemented MVC Presentation with Proxy Implementation

--- Microservices/Banking/Api/MicroRabbit.Banking.Api/Startup.cs
+++ Microservices/Banking/Api/MicroRabbit.Banking.Api/Startup.cs
@@ -57,9 +57,11 @@ namespace MicroRabbit.Banking.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseSwagger();
             app.UseSwaggerUI(c => {
--- Microservices/Transfer/Api/MicroRabbit.Transfer.Api/Startup.cs
+++ Microservices/Transfer/Api/MicroRabbit.Transfer.Api/Startup.cs
@@ -60,9 +60,11 @@ namespace MicroRabbit.Transfer.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseSwagger();
             app.UseSwaggerUI(c => {
